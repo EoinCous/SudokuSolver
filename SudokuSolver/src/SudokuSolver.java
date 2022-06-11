f60c824a752758745d8627a2508fca16d43ce8d2
Eoin <eoin229@gmail.com>
2022-06-11 14:49:24 +0100

    Added comments to the program

--- SudokuSolver/src/SudokuSolver.java
+++ SudokuSolver/src/SudokuSolver.java
@@ -1,3 +1,4 @@
+//This program solves any sudoku board
 
 public class SudokuSolver {
 	
@@ -5,6 +6,7 @@ public class SudokuSolver {
 
 	public static void main(String[] args) {
 		
+		//Sample unsolved board
 		int[][] board = {
 				{7,0,2,0,5,0,6,0,0},
 				{0,0,0,0,0,3,0,0,0},
@@ -17,8 +19,10 @@ public class SudokuSolver {
 				{0,0,7,0,4,0,2,0,3}
 		};
 		
+		//Prints the unsolved board
 		printBoard(board);
 		
+		//Runs algorithm to solve board
 		if(solveBoard(board)) {
 			System.out.println("Solved successfully");
 		}
@@ -26,9 +30,11 @@ public class SudokuSolver {
 			System.out.println("Unsolvable board");
 		}
 		
+		//Prints solved board
 		printBoard(board);
 	}
 	
+	//Displays board in a sudoku format
 	private static void printBoard(int[][] board) {
 		for(int row = 0; row<GRID_SIZE; row++) {
 			if(row %3 == 0 && row != 0) {
@@ -43,14 +49,17 @@ public class SudokuSolver {
 			System.out.println();
 		}
 	}
-	
+	/*
+	 * Method to easily enter an unsolved board
 	private static int[][] enterBoard() {
 		int[][] board;
 		
 		
 		return board;
 	}
-
+*/
+	//The following three methods check if a number is in the specified row/column/box
+	
 	private static boolean isNumberInRow(int[][] board, int number, int row) {
 		for(int i = 0; i < GRID_SIZE; i++) {
 			if(board[row][i] == number) {
@@ -83,12 +92,14 @@ public class SudokuSolver {
 		return false;
 	}
 	
+	//Checks if a number can be legally placed in that place
 	private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
 		return !isNumberInRow(board, number, row) &&
 				!isNumberInColumn(board, number, column) &&
 				!isNumberInBox(board, number, row, column);
 	}
 	
+	//Recursively solves the board by entering a valid number
 	private static boolean solveBoard(int[][] board) {
 		for(int row = 0; row < GRID_SIZE; row++) {
 			for(int column = 0; column < GRID_SIZE; column++) {
